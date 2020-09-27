04fdfb26ad5cec1f64e905a81cdd4e11c0b5c27a
sangyeNji <christiansangye@gmail.com>
2020-09-27 01:02:41 +0100

    adding all necessery components like sidebar, header, footer, table.

--- night-app/src/app/app-routing.module.ts
+++ night-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TableComponent } from './table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sidebar', component: SidebarComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'table', component: TableComponent},
+  {path: 'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- night-app/src/app/app.module.ts
+++ night-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
